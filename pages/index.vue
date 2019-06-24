0a34bbb3588b5eededba599e54f72ced30db66ac
okadato <>
2019-06-24 13:37:32 +0900

    lint

--- pages/index.vue
+++ pages/index.vue
@@ -2,23 +2,16 @@
   <section>
     <h1 class="header">Nuxt TypeScript Starter</h1>
     <div class="cards">
-      <Card
-        v-for="person in people"
-        :key="person.id"
-        :person="person"
-      ></Card>
+      <Card v-for="person in people" :key="person.id" :person="person"></Card>
     </div>
   </section>
 </template>
 
 <script lang="ts">
-import {
-  Component,
-  Vue
-} from "nuxt-property-decorator"
-import { State } from "vuex-class"
+import { Component, Vue } from "nuxt-property-decorator";
+import { State } from "vuex-class";
 import { Person } from "~/types";
-import Card from "~/components/Card.vue"
+import Card from "~/components/Card.vue";
 
 @Component({
   components: {
@@ -26,7 +19,7 @@ import Card from "~/components/Card.vue"
   }
 })
 export default class extends Vue {
-  @State people!: Person
+  @State people!: Person;
 }
 </script>
 
